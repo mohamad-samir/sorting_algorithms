5e64216cd8633e559e242331323bbb7006149f8d
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-23 13:50:53 +0300

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,18 +22,24 @@ void swap_ints(int *a, int *b)
  *
  * Description: Prints the array after each swap.
  */
-void selectionSort(int *array, size_t size)
+void selection_sort(int *array, size_t size)
 {
-	int i, j, min;
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
+		min = array + i;
 		for (j = i + 1; j < size; j++)
-			if (array[j] < array[min])
-				min = j;
+			min = (array[j] < *min) ? (array + j) : min;
 
-		swap_ints(array + i, min);
-		print_array(array, size);
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
 	}
 }
