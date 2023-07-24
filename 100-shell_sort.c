05e53fcfc5e3799752d06f97893356c4826dffbd
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-24 22:21:27 +0300

    update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -15,34 +15,43 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * shell_sort - Sort an array of integers in ascending
- *              order using the shell sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
+ * shell_sort - Sorts an array of integers in ascending order using
+ * the Shell sort algorithm with the Knuth sequence
+ * @array: The array of integers to be sorted
+ * @size: The size of the array
  *
- * Description: Uses the Knuth interval sequence.
+ * Return: Nothing
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t gap = 1;
+	size_t i, j;
+	int temp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	for (gap = 1; gap < (size / 3);)
+	/* Calculate the initial gap value using the Knuth sequence */
+	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
-	for (; gap >= 1; gap /= 3)
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
+			temp = array[i];
 			j = i;
-			while (j >= gap && array[j - gap] > array[j])
+
+			while (j >= gap && array[j - gap] > temp)
 			{
-				swap_ints(array + j, array + (j - gap));
+				swap_ints(&array[j], &array[j - gap]);
 				j -= gap;
 			}
+
+			array[j] = temp;
 		}
+
+		/* Print the array after each decrease in the gap */
 		print_array(array, size);
+
+		/* Update the gap value using the Knuth sequence */
+		gap = (gap - 1) / 3;
 	}
 }
