9e00102d658e79c032c2487f552ad1c709b4599a
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-24 22:32:08 +0300

    update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,9 +7,7 @@
  */
 void swap_ints(int *a, int *b)
 {
-	int tmp;
-
-	tmp = *a;
+	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
@@ -24,25 +22,35 @@ void swap_ints(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
-
 	if (array == NULL || size < 2)
 		return;
 
-	for (gap = 1; gap < (size / 3);)
+	size_t gap, i, j;
+
+	/* Calculate the initial gap using Knuth's sequence */
+	for (gap = 1; gap < size / 3;)
 		gap = gap * 3 + 1;
 
 	for (; gap >= 1; gap /= 3)
 	{
+		/* Perform insertion sort with the current gap */
 		for (i = gap; i < size; i++)
 		{
+			int current_element = array[i];
 			j = i;
-			while (j >= gap && array[j - gap] > array[j])
+
+			/* Shift elements to the right to create space for insertion */
+			while (j >= gap && array[j - gap] > current_element)
 			{
-				swap_ints(array + j, array + (j - gap));
+				array[j] = array[j - gap];
 				j -= gap;
 			}
+
+			/* Insert the current element into the correct position */
+			array[j] = current_element;
 		}
+
+		/* Print the array after each pass */
 		print_array(array, size);
 	}
 }
